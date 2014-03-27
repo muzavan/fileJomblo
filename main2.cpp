d61404a4994dc2a05130971a398abe3260298132
Muhammad Reza Irvanda <muzavan@gmail.com>
2014-03-27 09:37:59 +0700

    Lebih sopan Hi dibanding Hello

--- main2.cpp
+++ main2.cpp
@@ -1,10 +1,10 @@
-//File : main.cpp
+//File : main2.cpp
 //Author : 13512042 - Muhammad Reza Irvanda
 
 #include <iostream>
 using namespace std;
 
 int main(){
-	cout<<"Hello Git!"<<endl;
+	cout <<"Hi git!"<<endl;
 	return 0;
 }
